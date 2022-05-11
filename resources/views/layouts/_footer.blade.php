e408a02672bc47371850163dab4048023037c661
hadson <vtaochao@msn.com>
2022-05-11 04:16:43 +0000

    基础页面构建

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container">
+        <p class="float-left"> 由
+            <a href="#" target="_blank">Summer</a>
+            设计和编码
+        </p>
+        <p class="float-right"><a href="mailto:name@email.com">联系我们</a></p>
+    </div>
+</footer>
