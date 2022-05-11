e573fcbde250281ce1c039330b6cce4e370a8832
hadson <vtaochao@msn.com>
2022-05-11 04:21:20 +0000

    静态文件浏览器缓存问题

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="footer">
     <div class="container">
         <p class="float-left"> 由
-            <a href="#" target="_blank">Summer</a>
+            <a href="#" target="_blank">Hadson</a>
             设计和编码
         </p>
         <p class="float-right"><a href="mailto:name@email.com">联系我们</a></p>
