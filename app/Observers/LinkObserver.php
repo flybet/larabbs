a1e89dca9ac8a1c81b6bd2109f89b9ea90e35bca
hadson <vtaochao@msn.com>
2022-06-28 06:23:49 +0000

    侧边栏链接

---
+++ app/Observers/LinkObserver.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chaotao
+ * Date: 2022/6/28
+ * Time: 14:19
+ */
+
+namespace App\Observers;
+
+
+class LinkObserver
+{
+
+}
---
+++ config/administrator/links.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chaotao
+ * Date: 2022/6/28
+ * Time: 14:13
+ */
