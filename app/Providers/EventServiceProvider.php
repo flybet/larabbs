d2cc8cb9b91befb5bbaab658f00a6b8e744ea1be
hadson <vtaochao@msn.com>
2022-07-07 07:13:10 +0000

    add socialiteproivders

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -20,7 +20,11 @@ class EventServiceProvider extends ServiceProvider
         ],
         \Illuminate\Auth\Events\Verified::class=>[
             \App\Listeners\EmailVerified::class,
-        ]
+        ],
+        \SocialiteProviders\Manager\SocialiteWasCalled::class => [
+            // add your listeners (aka providers) here
+            'SocialiteProviders\\Weixin\\WeiXinExtendSocialite@handle',
+        ],
     ];
 
     /**
